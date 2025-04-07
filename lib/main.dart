0709619648c2e87d91fbec3571db1bde0910de9f
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-08 01:01:47 +0200

    Add dotenv and modal progress HUD dependencies; refactor ConfirmLoginScreen layout with loading indicator

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:alrefadah/my_app.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-
+  await dotenv.load();
   // Set the preferred orientations to portrait only
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
