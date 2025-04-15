bcfbad740626caeacd4dea8e4a0b2d763aeac2f3
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-15 12:01:07 +0200

    Add AppNavigationBar: & SelectIconStyle, and TabItemName components

---
+++ lib/screens/navigation_bar/widgets/tab_item_name.dart
@@ -0,0 +1,21 @@
+import 'package:alrefadah/core/utils/constants/colors_constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TabItemName extends StatelessWidget {
+  const TabItemName({required this.title, super.key});
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(
+        color: kMainColor,
+        fontSize: 13.sp,
+        fontFamily: 'GE SS Two',
+        fontWeight: FontWeight.w500,
+        height: 1.54.h,
+      ),
+    );
+  }
+}
