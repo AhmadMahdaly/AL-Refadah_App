b6a58d1e59b8c74797e654a4d59c3c9b6c7f6f6f
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-05-07 01:17:49 +0300

    Refactor main.dart to initialize app using a new method and remove MyApp class. Simplify app startup process by eliminating localization and dotenv dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,9 @@
-import 'package:alrefadah/my_app.dart';
-import 'package:easy_localization/easy_localization.dart';
+import 'package:alrefadah/core/init/initializer.dart';
+import 'package:alrefadah/presentation/app/my_app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
-  await dotenv.load();
-  // Set the preferred orientations to portrait only
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]).then((_) {
-    runApp(
-      EasyLocalization(
-        supportedLocales: const [Locale('en'), Locale('ar')],
-        path: 'assets/translations',
-        startLocale: const Locale('ar'),
-        child: const MyApp(),
-      ),
-    );
-  });
+  await initializeApp();
+
+  runApp(const MyApp());
 }
