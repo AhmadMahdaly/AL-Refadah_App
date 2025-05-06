ebcc6ff5c7d8508fa933486f34f9a7e032a073b1
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-05-07 01:20:02 +0300

    Add colors_constants.dart: Define color constants for theming, including background, text, and error colors.

---
+++ lib/core/themes/colors_constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const kScaffoldBackgroundColor = Color(0xFFFEFEFE);
+const kMainColor = Color(0xFFA80713);
+const kSecondaryColor = Color(0xFF690109);
+const kTextColor = Color(0xFF494949);
+const kMainColorLightColor = Color(0xFFEFE5F9);
+const kUnselectedIconColor = Color(0xffA2A2A2);
+const kShadowColor = Color(0x0C000000);
+const kMainExtrimeLightColor = Color(0xFFF5F5F5);
+const kDartTextColor = Color(0xFF150000);
+const kGrayColor = Color(0xFF303030);
+const kAnalysisMediumColor = Color(0xFFA2A2A2);
+const kErrorColor = Color(0xffEA1C25);
+const kGreenColor = Color(0xFF54A700);
