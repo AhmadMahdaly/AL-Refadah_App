7d4e6d4fcb9a0185d190cdf7759e4352900c5eb5
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-05-07 01:30:28 +0300

    Add constants_variable.dart for app-wide constants: Introduce a new file containing constant variables, including company ID and a mapping for Arabic to Latin numbers, along with a function for conversion to streamline access and modifications.

---
+++ lib/data/constants_variable.dart
@@ -0,0 +1,28 @@
+/// This file contains all the constants variables used in the app
+// for easy access and modification.
+
+library;
+
+///  This is the ID of the company used in the app.
+const int companyId = 2;
+
+///
+const Map<String, String> arabicToLatinNumberMap = {
+  '1': '١',
+  '2': '٢',
+  '3': '٣',
+  '4': '٤',
+  '5': '٥',
+  '6': '٦',
+  '7': '٧',
+  '8': '٨',
+  '9': '٩',
+  '0': '٠',
+};
+String convertArabicToLatin(String input) {
+  var result = input.toLowerCase();
+  arabicToLatinNumberMap.forEach((latin, arabic) {
+    result = result.replaceAll(arabic, latin);
+  });
+  return result;
+}
