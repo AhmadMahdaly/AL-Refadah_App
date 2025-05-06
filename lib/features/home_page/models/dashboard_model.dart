5b5efe2ea7627233131112f552276c942251b8d2
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-05-07 01:33:51 +0300

    Add DashboardModel and HomeSeasonModel classes: Introduce DashboardModel for managing dashboard data and HomeSeasonModel for handling seasonal information, including JSON serialization methods. This enhances data management for the home page features.

---
+++ lib/features/home_page/models/dashboard_model.dart
@@ -0,0 +1,63 @@
+class DashboardModel {
+  DashboardModel({
+    required this.totalStageBuses,
+    required this.totalStageTrips,
+    required this.totalDepartingBuses,
+    required this.totalTrips,
+    required this.totalFinishedBuses,
+    required this.totalPilgrims,
+    required this.totalStagePilgrims,
+    required this.totalEvacueesPilgrims,
+    required this.totalArrivedPilgrims,
+    required this.totalRemainingPilgrims,
+    required this.totalFinishedCenters,
+    required this.totalRemainingCenterPilgrims,
+  });
+
+  factory DashboardModel.fromJson(Map<String, dynamic> json) {
+    return DashboardModel(
+      totalStageBuses: json['totalStageBuses'] as int,
+      totalStageTrips: json['totalStageTrips'] as int,
+      totalDepartingBuses: json['totaldepartingBuses'] as int,
+      totalTrips: json['totalTrips'] as int,
+      totalFinishedBuses: json['totalFinishedBuses'] as int,
+      totalPilgrims: json['totalPilgrims'] as int,
+      totalStagePilgrims: json['totalStagePilgrims'] as int,
+      totalEvacueesPilgrims: json['totalEvacueesPilgrims'] as int,
+      totalArrivedPilgrims: json['totalArrivedPilgrims'] as int,
+      totalRemainingPilgrims: json['totalRemainigPilgrims'] as int,
+      totalFinishedCenters: json['totalFinishedCenters'] as int,
+      totalRemainingCenterPilgrims: json['totalRemainingCenterPilgrims'] as int,
+    );
+  }
+
+  final int totalStageBuses;
+  final int totalStageTrips;
+  final int totalDepartingBuses;
+  final int totalTrips;
+  final int totalFinishedBuses;
+  final int totalPilgrims;
+  final int totalStagePilgrims;
+  final int totalEvacueesPilgrims;
+  final int totalArrivedPilgrims;
+  final int totalRemainingPilgrims;
+  final int totalFinishedCenters;
+  final int totalRemainingCenterPilgrims;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'totalStageBuses': totalStageBuses,
+      'totalStageTrips': totalStageTrips,
+      'totaldepartingBuses': totalDepartingBuses,
+      'totalTrips': totalTrips,
+      'totalFinishedBuses': totalFinishedBuses,
+      'totalPilgrims': totalPilgrims,
+      'totalStagePilgrims': totalStagePilgrims,
+      'totalEvacueesPilgrims': totalEvacueesPilgrims,
+      'totalArrivedPilgrims': totalArrivedPilgrims,
+      'totalRemainigPilgrims': totalRemainingPilgrims,
+      'totalFinishedCenters': totalFinishedCenters,
+      'totalRemainingCenterPilgrims': totalRemainingCenterPilgrims,
+    };
+  }
+}
