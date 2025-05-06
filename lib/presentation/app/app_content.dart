b7acc25e09fc27434de04f21b78984c62b91e82b
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-05-07 01:31:19 +0300

    Add AppContent and MyApp widgets: Introduce AppContent for managing app state and layout, and MyApp as the root widget to set up the app's structure, including localization and theme configuration. Remove CheckAuth widget for improved authentication flow management.

---
+++ lib/presentation/app/app_content.dart
@@ -0,0 +1,35 @@
+import 'package:alrefadah/core/init/build_bloc_providers.dart';
+import 'package:alrefadah/core/themes/app_theme.dart';
+import 'package:alrefadah/core/themes/unfocus_scope.dart';
+import 'package:alrefadah/features/splash_screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+/// A widget that handles the app's content and state management.
+class AppContent extends StatelessWidget {
+  const AppContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => unfocusScope(context),
+      child: MultiBlocProvider(
+        providers: buildBlocProviders(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'AL Rifadah',
+          theme: appTheme(context),
+          locale: const Locale('ar'), // هذا يحدد اللغة (والاتجاه rtl)
+          supportedLocales: const [Locale('ar')],
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          home: const SplashScreen(),
+        ),
+      ),
+    );
+  }
+}
