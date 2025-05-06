d86bcc2becf386ff2d0d0c8f91cab11c7a2a2dfb
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-05-07 01:24:37 +0300

    Refactor authentication logic: Remove old AuthCubit and AuthStates, and implement a new AuthCubit with updated login, registration, and verification methods, along with corresponding states for improved user authentication flow.

---
+++ lib/features/auth/cubit/auth_cubit.dart
@@ -0,0 +1,178 @@
+import 'dart:async';
+
+import 'package:alrefadah/core/services/cache_helper.dart';
+import 'package:alrefadah/core/services/dio_helper.dart';
+import 'package:alrefadah/data/constants_variable.dart';
+import 'package:alrefadah/features/auth/cubit/auth_states.dart';
+import 'package:alrefadah/features/auth/models/user_register_model.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class AuthCubit extends Cubit<AuthStates> {
+  AuthCubit() : super(LoginInitialState());
+
+  static const storage = FlutterSecureStorage();
+  Future<void> login({
+    required String phoneNo,
+    required String password,
+  }) async {
+    try {
+      emit(LoginLoadingState());
+      await CacheHelper.saveData(key: 'phoneNo', value: phoneNo);
+      await CacheHelper.saveData(key: 'password', value: password);
+      final response = await DioHelper.dio.post<Map<String, dynamic>>(
+        '/Authentication/login',
+        data: {
+          'loginParameter': phoneNo,
+          'password': password,
+          'companyId': companyId,
+        },
+      );
+      await CacheHelper.saveData(key: 'phoneNo', value: phoneNo);
+      await CacheHelper.saveData(key: 'password', value: password);
+      if (response.statusCode == 200 && response.data!['success'] == true) {
+        final userId = response.data!['userId'];
+        await CacheHelper.saveData(key: 'userId', value: userId);
+
+        emit(LoginSuccessState());
+      } else {
+        emit(LoginErrorState('فشل تسجيل الدخول'));
+      }
+    } on DioException catch (e) {
+      final message =
+          (e.response?.data as Map<String, dynamic>?)?['message']?.toString() ??
+          'حدث خطأ';
+      emit(LoginErrorState(message));
+    } catch (e) {
+      emit(LoginErrorState('حدث خطأ غير متوقع'));
+    }
+  }
+
+  Future<void> loginWithCode({required String code}) async {
+    emit(LoginWithCodeLoadingState());
+    final userId = await CacheHelper.getData(key: 'userId');
+    try {
+      final response = await DioHelper.dio.post<Map<String, dynamic>>(
+        '/Authentication/loginWithCode',
+        data: {'code': code, 'userId': userId},
+      );
+
+      if (response.statusCode == 200 && response.data!['success'] == true) {
+        final accessToken = response.data!['accessToken'];
+        // final refreshToken = response.data!['refreshToken'];
+
+        await CacheHelper.saveData(key: 'accessToken', value: accessToken);
+        // await CacheHelper.saveData(key: 'refreshToken', value: refreshToken);
+        await DioHelper.init();
+
+        emit(LoginWithCodeSuccessState());
+      } else {
+        emit(LoginWithCodeErrorState('فشل تسجيل الدخول'));
+      }
+    } on DioException catch (e) {
+      final message =
+          (e.response?.data as Map<String, dynamic>?)?['message']?.toString() ??
+          'حدث خطأ';
+      emit(LoginWithCodeErrorState(message));
+    } catch (e) {
+      emit(LoginWithCodeErrorState('حدث خطأ غير متوقع'));
+    }
+  }
+
+  Future<void> logout() async {
+    await storage.deleteAll();
+    emit(LogoutSuccessState());
+  }
+
+  Future<void> register(UserRegisterModel userRegisterModel) async {
+    try {
+      emit(RegisterLoadingState());
+
+      final response = await DioHelper.dio.post<Map<String, dynamic>>(
+        '/Authentication/register',
+        data: userRegisterModel.toJson(),
+      );
+      await CacheHelper.saveData(
+        key: 'phoneNo',
+        value: userRegisterModel.fJawNo,
+      );
+      await CacheHelper.saveData(
+        key: 'password',
+        value: userRegisterModel.fUserPass,
+      );
+      if (response.statusCode == 200) {
+        final userId = response.data!['fUserId'];
+        await CacheHelper.saveData(key: 'userId', value: userId);
+        emit(RegisterSuccessState());
+      } else {
+        emit(RegisterErrorState('فشل إنشاء الحساب'));
+      }
+    } catch (e) {
+      emit(RegisterErrorState('حدث خطأ غير متوقع'));
+    }
+  }
+
+  Future<void> verifyRegister(String code) async {
+    try {
+      emit(VerifyRegisterLoadingState());
+      final userId = await CacheHelper.getData(key: 'userId');
+      final response = await DioHelper.dio.post<Map<String, dynamic>>(
+        '/Authentication/verifyRegister',
+        data: {'userId': userId, 'code': code},
+      );
+
+      if (response.statusCode == 200) {
+        emit(VerifyRegisterSuccessState());
+      } else {
+        emit(VerifyRegisterErrorState('فشل إنشاء الحساب'));
+      }
+    } catch (e) {
+      emit(VerifyRegisterErrorState('حدث خطأ غير متوقع'));
+    }
+  }
+
+  Future<void> resendOTP() async {
+    try {
+      emit(ResendOTPLoadingState());
+      final phoneNo = await CacheHelper.getData(key: 'phoneNo');
+      final password = await CacheHelper.getData(key: 'password');
+      final response = await DioHelper.dio.post<Map<String, dynamic>>(
+        '/Authentication/ResendOTP',
+        data: {
+          'loginParameter': phoneNo,
+          'password': password,
+          'companyId': companyId,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final userId = response.data!['userId'];
+        await CacheHelper.saveData(key: 'userId', value: userId);
+        emit(ResendOTPSuccessState());
+      } else {
+        emit(ResendOTPErrorState('فشل إعادة إرسال الرمز'));
+      }
+    } catch (e) {
+      emit(ResendOTPErrorState('حدث خطأ غير متوقع'));
+    }
+  }
+
+  Future<void> verifyLogin(String code) async {
+    try {
+      emit(VerifyLoginLoadingState());
+
+      final response = await DioHelper.dio.post<Map<String, dynamic>>(
+        '/Authentication/ConfirmLogin?userId=$code&companyId=$companyId',
+      );
+
+      if (response.statusCode == 200) {
+        emit(VerifyLoginSuccessState());
+      } else {
+        emit(VerifyLoginErrorState('فشل إنشاء الحساب'));
+      }
+    } catch (e) {
+      emit(VerifyLoginErrorState('حدث خطأ غير متوقع'));
+    }
+  }
+}
---
+++ lib/features/auth/cubit/auth_states.dart
@@ -0,0 +1,93 @@
+abstract class AuthStates {}
+
+class LoginInitialState extends AuthStates {}
+
+class LoginLoadingState extends AuthStates {}
+
+class LoginSuccessState extends AuthStates {}
+
+class LoginErrorState extends AuthStates {
+  LoginErrorState(this.message);
+  final String message;
+}
+
+///Logout
+class LogoutLoadingState extends AuthStates {}
+
+class LogoutSuccessState extends AuthStates {}
+
+class LogoutErrorState extends AuthStates {
+  LogoutErrorState(this.message);
+  final String message;
+}
+
+///Login with code
+class LoginWithCodeLoadingState extends AuthStates {}
+
+class LoginWithCodeSuccessState extends AuthStates {}
+
+class LoginWithCodeErrorState extends AuthStates {
+  LoginWithCodeErrorState(this.message);
+  final String message;
+}
+
+///
+final class SetUpLoadingState extends AuthStates {}
+
+final class SetUpSuccessState extends AuthStates {}
+
+final class SetUpErrorState extends AuthStates {}
+
+///
+final class RefreshTokenLoadingState extends AuthStates {}
+
+final class RefreshTokenSuccessState extends AuthStates {}
+
+final class RefreshTokenErrorState extends AuthStates {}
+
+///
+final class IsLoginLoadingState extends AuthStates {}
+
+final class IsLoginSuccessState extends AuthStates {}
+
+final class IsLoginErrorState extends AuthStates {}
+
+///
+final class RegisterLoadingState extends AuthStates {}
+
+final class RegisterSuccessState extends AuthStates {}
+
+final class RegisterErrorState extends AuthStates {
+  RegisterErrorState(this.message);
+  final String message;
+}
+
+///
+final class VerifyRegisterLoadingState extends AuthStates {}
+
+final class VerifyRegisterSuccessState extends AuthStates {}
+
+final class VerifyRegisterErrorState extends AuthStates {
+  VerifyRegisterErrorState(this.message);
+  final String message;
+}
+
+///
+final class ResendOTPLoadingState extends AuthStates {}
+
+final class ResendOTPSuccessState extends AuthStates {}
+
+final class ResendOTPErrorState extends AuthStates {
+  ResendOTPErrorState(this.message);
+  final String message;
+}
+
+///
+final class VerifyLoginLoadingState extends AuthStates {}
+
+final class VerifyLoginSuccessState extends AuthStates {}
+
+final class VerifyLoginErrorState extends AuthStates {
+  VerifyLoginErrorState(this.message);
+  final String message;
+}
