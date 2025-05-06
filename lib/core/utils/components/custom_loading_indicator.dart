f22da233c0d4ae7065444ed527c24b4978578aac
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-05-07 01:21:10 +0300

    Add custom_loading_indicator.dart: Implement AppIndicator widget using SpinKit for a customizable loading indicator with theming support.

---
+++ lib/core/utils/components/custom_loading_indicator.dart
@@ -0,0 +1,15 @@
+import 'package:alrefadah/core/themes/colors_constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class AppIndicator extends StatelessWidget {
+  const AppIndicator({super.key, this.size = 50});
+  final double size;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SpinKitFadingCube(size: size.sp, color: kMainColor.withAlpha(70)),
+    );
+  }
+}
